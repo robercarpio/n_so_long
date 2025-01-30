1010c5b8d5796740aea7fa34f3e5a0b5e7f2459d
Rober Carpio <101320752+robercarpio@users.noreply.github.com>
2025-01-30 16:14:22 +0100

    .

--- aux/fill_down.c
+++ aux/fill_down.c
@@ -5,10 +5,10 @@ int	fill_down(char **map, int y, int x)
     int height = map_height(map);
     while (y < height - 1 && map[y + 1][x] != '1')
     {
-	if(map[y + 1][x] != 'E')
-		return(1);
-	map[y + 1][x] = 'V';
-	y++;
+        if(map[y + 1][x] != 'E')
+            return(1);
+        map[y + 1][x] = 'V';
+        y++;
     }
     return(0);
 }
